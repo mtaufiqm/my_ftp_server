f03d904f227fb5a2953debf6333ba2313a676049
Anonymous Indonesia <muhtaufiqmukhtar@gmail.com>
2025-03-13 09:58:01 +0800

    13032025

---
+++ bin/my_ftp_server.dart
@@ -0,0 +1,39 @@
+
+import "dart:io";
+
+import "package:ftp_server/ftp_server.dart";
+import "package:ftp_server/server_type.dart";
+import "package:postgres/postgres.dart";
+Future<void> main(List<String> arguments) async {
+  String host = Platform.environment["dbhost"]??"localhost";
+  String databaseName = Platform.environment["dbname"]??"myapp";
+  String user = Platform.environment["dbuser"]??"postgres";
+  String password = Platform.environment["dbpassword"]??"taufiq1729";
+  Endpoint endpoint = Endpoint(host: host, database: databaseName,username: user,password: password);
+  Pool connPool = Pool.withEndpoints([endpoint],settings: PoolSettings(
+    maxConnectionCount: 3,
+    sslMode: SslMode.disable
+  ));
+  String ftpUser = "admin";
+  String ftpPass = "admin";
+  await connPool.runTx<void>((tx) async {
+    var result = await tx.execute(r"SELECT * FROM ftp_server");
+    if(result.isEmpty){
+      throw Exception("There is no Authenticated Data");
+    }
+    Map<String,dynamic> authMap = result.first.toColumnMap();
+    ftpUser = authMap["username"];
+    ftpPass = authMap["password"];
+  });
+
+  FtpServer ftpServer = FtpServer(
+    21, 
+    sharedDirectories: ["shared_dir"],
+    serverType: ServerType.readAndWrite,
+    username: ftpUser,
+    password: ftpPass,
+  );
+
+  print("FTP Started in Port 21...");
+  await ftpServer.start();
+}
